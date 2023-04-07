7b4fd3f85add83e436963fd3161b715e3cb85fb7
Tim-KILLER <abdulmanovtumur@mail.ru>
2023-04-07 13:06:19 +0300

    first commit

---
+++ vite-project/src/App.vue
@@ -0,0 +1,53 @@
+<script lang="ts">
+  export default{
+    data(){
+      return{
+        inputValue: "1:00",
+        selectValue: "",
+        recordList:[]
+      }
+    },
+
+    methods: {
+      inputСhanges(event:any){
+        this.selectValue = event.target.value
+      },
+      selectChanges(event:any){
+        this.inputValue = event.target.value
+      },
+      addRecord(){
+        const recordObj:{inputValue:String;selectValue:String} = {inputValue: this.inputValue,selectValue: this.selectValue}
+        this.recordList.push(recordObj)
+        console.log(typeof(this.recordList))
+        console.log(this.recordList)
+        
+      }
+    }
+  }
+
+
+</script>
+
+<template>
+  <div class = "body">
+    <div>
+      <select v-model="inputValue" @select="selectChanges" name="" id="">
+        <option
+          v-for="i in 22"
+          >{{ i }}:00
+        </option>
+      </select>
+      <input
+        v-bind:value="selectValue"
+        @input="inputСhanges"
+      />
+      <button @click="addRecord">add record</button>
+    </div>
+
+  </div>
+  
+</template>
+
+<style scoped>
+
+</style>
